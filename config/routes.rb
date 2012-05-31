df831d1cda5148289bc7e4e5ce4ec6aa75b057d6
Edward Weng <edweng@gmail.com>
2012-05-31 16:53:56 -0400

    Adding tests and api

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ ThinAuth::Application.routes.draw do
 
   root :to => "pages#index"
 
+  namespace :api do
+    namespace :v1 do
+      resources :users, :only => [:show]
+    end
+  end
+  
 end
---
+++ spec/requests/api/users_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+require 'rack/test'
+
+describe "service" do
+  describe "GET on /api/v1/users/:id" do 
+    
+    let(:user) { User.create(:name => "a", :email => "a@a.com", :password => "hungry") }
+
+    it "should return a user by name" do
+      get ('/api/v1/users/' + user.id.to_s)
+      response.status.should == 200
+      attributes = JSON.parse(response.body)
+      attributes["name"].should == "a"
+    end
+
+    it "should return a user with an email" do
+      get ('/api/v1/users/' + user.id.to_s)
+      response.status.should == 200
+      attributes = JSON.parse(response.body)
+      attributes["email"].should == "a@a.com"
+    end
+
+    it "should return a 404 for a user that doesn't exist" do
+      get '/api/v1/users/100'
+      response.status.should == 404
+    end 
+  end
+end
