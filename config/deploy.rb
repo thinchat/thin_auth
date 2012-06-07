da9029dd2cc2a0c1b5fecc52609dc0ddadbad858
Chris Anderson <christopher.anderson@gmail.com>
2012-06-07 17:31:15 -0400

    Added cap roles

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,37 +1,30 @@
 require "bundler/capistrano"
+require "capistrano/ext/multistage"
 
-server "50.116.34.44", :web, :app, :db, primary: true
-
-set :application, "thin_auth"
-set :user, "deployer"
-set :deploy_to, "/home/#{user}/apps/#{application}"
-set :deploy_via, :remote_cache
-set :use_sudo, false
-
-set :scm, "git"
-set :repository, "git@github.com:thinchat/#{application}.git"
-set :branch, "master"
+set :stages, %w(vagrant production)
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
-after "deploy", "deploy:cleanup" # keep only the last 5 releases
+after "deploy", "deploy:nginx_config", "deploy:cleanup" # keep only the last 5 releases
 
 namespace :deploy do
   %w[start stop restart].each do |command|
     desc "#{command} unicorn server"
     task command, roles: :app, except: {no_release: true} do
-      run "/etc/init.d/unicorn_#{application} #{command}"
+      sudo "/etc/init.d/unicorn_#{application} #{command}"
     end
   end
 
   task :setup_config, roles: :app do
+    puts "I AM LIKING THINGS"
     sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
     run "mkdir -p #{shared_path}/config"
     put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
     puts "Now edit the config files in #{shared_path}."
   end
   after "deploy:setup", "deploy:setup_config"
+
 
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
@@ -40,7 +33,7 @@ namespace :deploy do
 
   desc "Make sure local git is in sync with remote."
   task :check_revision, roles: :web do
-    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+    unless `git rev-parse HEAD` == `git rev-parse origin/#{branch}`
       puts "WARNING: HEAD is not the same as origin/master"
       puts "Run `git push` to sync changes."
       exit
---
+++ config/deploy/vagrant.rb
@@ -0,0 +1,15 @@
+set :host, "localhost"
+role :web, host
+role :app, host
+role :db, host, :primary => true
+ssh_options[:port] = "2222"
+
+set :application, "thin_auth"
+set :user, "deployer"
+set :deploy_to, "/home/#{user}/apps/#{application}"
+# set :deploy_via, :remote_cache
+set :use_sudo, false
+
+set :scm, "git"
+set :repository, "git@github.com:thinchat/#{application}.git"
+set :branch, "master"
