eeb922eb265cae7e3f866b13dd4b22bdacacd418
Chris Anderson <christopher.anderson@gmail.com>
2012-06-16 23:23:42 -0400

    fixing chris dumb mistake

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,22 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource_or_scope)
     cookies.delete :user
-    "http://thinchat.com"
+    current_root_url
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    "#{current_root_url}/rooms"
+  end
+
+  private
+
+  def current_root_url
+    if Rails.env.production?
+      "http://thinchat.com"
+    elsif Rails.env.development?
+      "http://localhost:3000"
+    else
+      "http://staging.thinchat.com"
+    end
   end
 end
