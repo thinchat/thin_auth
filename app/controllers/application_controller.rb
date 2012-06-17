61b6382570faf2f2d7a2bb31923af952cd3f273a
Chris Anderson <christopher.anderson@gmail.com>
2012-06-16 23:09:07 -0400

    grr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource_or_scope)
     cookies.delete :user
-    redirect_to "http://thinchat.com" and return
+    redirect_to "http://thinchat.com"
   end
 end
